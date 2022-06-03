63b080f7de6ac24e0631e304b0efc35bd590f2f7
Elvira Antonovskaya <sabirova.elvira.gam@gmail.com>
2022-06-03 16:12:39 +0500

    refactor: useContext in Card and use for delete-button

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,12 +1,21 @@
 import React from 'react';
+import {CurrentUserContext} from "../contexts/CurrentUserContext";
+import {useContext} from "@types/react";
 
 function Card({card, onCardClick}) {
     function handleClick() {
         onCardClick(card);
     }
+
+    const currentUser = useContext(CurrentUserContext)
+    const isOwn = card.owner._id === currentUser._id;
+
+    const cardDeleteButtonClassName = (
+        `card__delete-button ${isOwn ? 'card__delete-button_visible' : 'card__delete-button_hidden'}`
+    );
     return (
         <li onClick={handleClick} className="cards__item card">
-            <button type="button" className="hidden card__button card__button_delete"></button>
+            <button type="button" className={cardDeleteButtonClassName}></button>
             <img src={card.link} alt={card.name} className="card__img"/>
             <div className="card__body">
                 <h2 className="card__title">{card.name}</h2>
