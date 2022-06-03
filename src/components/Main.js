3a0cae5a9a7e4d4525155660ea8a13b74cfb6afa
Elvira Antonovskaya <sabirova.elvira.gam@gmail.com>
2022-06-03 15:59:01 +0500

    refactor: use CurrentUserContext at Main

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,24 +3,25 @@ import avatar from '../img/image.jpg';
 import editIcon from '../img/edit.svg';
 import addIcon from '../img/add_button.svg';
 import React from 'react';
-import {useEffect, useState} from 'react'
+import {useEffect, useState, useContext} from 'react'
 import api from '../utils/Api';
 import Card from './Card'
+import {CurrentUserContext} from "../contexts/CurrentUserContext";
 
 function Main({onEditAvatar, onEditProfile, onAddPlace, onClose, onCardClick}) {
 
     const [cards, setCards] = useState([])
-    const [userAvatar, setUserAvatar] = useState('')
-    const [userName, setUserName] = useState('ella')
-    const [userDescription, setUserDescription] = useState('ella')
-
+    // const [userAvatar, setUserAvatar] = useState('')
+    // const [userName, setUserName] = useState('ella')
+    // const [userDescription, setUserDescription] = useState('ella')
+    const currentUser = useContext(CurrentUserContext)
 
     useEffect(() => {
-        Promise.all([api.getUserData(), api.getInitialCards()])
+        api.getInitialCards()
             .then(([userData, cards]) => {
-                setUserAvatar(userData.avatar);
-                setUserName(userData.name);
-                setUserDescription(userData.about);
+                // setUserAvatar(currentUser.avatar);
+                // setUserName(currentUser.name);
+                // setUserDescription(currentUser.about);
 
                 setCards(cards.map(item => ({
                     name: item.name,
@@ -42,17 +43,17 @@ function Main({onEditAvatar, onEditProfile, onAddPlace, onClose, onCardClick}) {
             <section className="profile">
                 <button onClick={onEditAvatar} className="button profile__button profile__button_type_change-avatar">
                     <img src={editProfileIcon} alt="иконка редактирования профиля" className="profile__icon profile__icon_type_edit-avatar"/>
-                    <img src={userAvatar ? userAvatar : avatar} alt="фото пользователя" className="profile__avatar"/>
+                    <img src={currentUser.avatar ? currentUser.avatar : avatar} alt="фото пользователя" className="profile__avatar"/>
                 </button>
                 <div className="profile__info">
                     <div className="profile__name-block">
-                        <h1 className="profile__name">{userName} </h1>
+                        <h1 className="profile__name">{currentUser.name} </h1>
                         <button onClick={onEditProfile} className="button profile__button profile__button_type_edit">
                             <img className="profile__icon profile__icon_type_edit" src={editIcon}
                                  alt="иконка редактирование"/>
                         </button>
                     </div>
-                    <p className="profile__description">{userDescription}</p>
+                    <p className="profile__description">{currentUser.about}</p>
                 </div>
                 <button onClick={onAddPlace} className="button profile__button profile__button_type_add">
                     <img className="profile__icon profile__icon_type_add" src={addIcon} alt="плюс"/>
